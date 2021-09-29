f6b930a5283ee0901acd98d94ca7c26154cfa2dd
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-29 17:13:01 +0530

    added delete action

--- src/Context/GlobalState.js
+++ src/Context/GlobalState.js
@@ -14,8 +14,17 @@ const initialState = {
  export const GlobalProvider = ({ children }) => {
      const [state, dispatch] = useReducer(AppReducer, initialState);
 
+     //Actions
+     function deleteTransaction(id){
+         dispatch({
+             type: 'DELETE_TRANSACTION',
+             payload: id
+         })
+     }
+
      return (<GlobalContext.Provider value={{
-         transaction: state.transaction
+         transaction: state.transaction,
+         deleteTransaction
      }}>
          {children}
      </GlobalContext.Provider>)
