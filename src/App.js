f799c8a57885b1021c17557ef2988059083c2140
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-29 10:33:48 +0530

    added IncomExpenses

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Balance from './Components/Balance';
 import Header from './Components/Header';
+import IncomeExpenses from './Components/IncomeExpenses';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header/>
       <div className="container">
         <Balance/>
+        <IncomeExpenses/>
       </div>
     </div>
   );
