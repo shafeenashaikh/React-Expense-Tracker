a350c699c26878d8565e02ce98bf655a3885560b
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-29 10:21:00 +0530

    created Hedaer and Balance info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Balance from './Components/Balance';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-    hello
+    <div>
+      <Header/>
+      <div className="container">
+        <Balance/>
+      </div>
     </div>
   );
 }
